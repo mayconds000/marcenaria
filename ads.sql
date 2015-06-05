97edafc0f7147093abbd0f39051892cf1ddd4006
Maycon <mayconds000@hotmail.com>
2015-06-05 18:00:35 -0300

    subindo arquivo sql atualizado

--- ads.sql
+++ ads.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50617
 File Encoding         : 65001
 
-Date: 2015-04-22 17:06:22
+Date: 2015-06-05 17:59:51
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -29,7 +29,7 @@ CREATE TABLE `buy` (
   PRIMARY KEY (`id`),
   KEY `fk_buy_supplier` (`fornecedor_id`),
   CONSTRAINT `fk_buy_supplier` FOREIGN KEY (`fornecedor_id`) REFERENCES `supplier` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=26 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=29 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of buy
@@ -45,7 +45,6 @@ INSERT INTO `buy` VALUES ('8', '4', '2015-04-06', '2015-04-08', '156848', '2300.
 INSERT INTO `buy` VALUES ('9', '3', '2015-04-08', '2015-04-08', '165895', '157.95');
 INSERT INTO `buy` VALUES ('10', '5', '2015-04-13', '2015-04-08', '156465', '36559.98');
 INSERT INTO `buy` VALUES ('11', '1', '2015-05-01', '2015-04-17', '14689', '175.49');
-INSERT INTO `buy` VALUES ('12', '3', '2015-05-04', '2015-04-17', '1354689', '3333.33');
 INSERT INTO `buy` VALUES ('13', '3', '2015-04-02', '2015-04-17', '213213', '985.45');
 INSERT INTO `buy` VALUES ('14', '1', '2015-04-01', '2015-04-17', '7864489', '125.35');
 INSERT INTO `buy` VALUES ('15', '2', '2015-04-01', '2015-04-17', '7864489', '43299.00');
@@ -54,11 +53,13 @@ INSERT INTO `buy` VALUES ('17', '3', '2015-04-02', '2015-04-17', '213213', '7968
 INSERT INTO `buy` VALUES ('18', '2', '2015-04-01', '2015-04-17', '7864489', '174.23');
 INSERT INTO `buy` VALUES ('19', '2', '2015-04-01', '2015-04-17', '7864489', '16855.00');
 INSERT INTO `buy` VALUES ('20', '2', '2015-04-01', '2015-04-17', '7864489', '2240.35');
-INSERT INTO `buy` VALUES ('21', '2', '2015-03-05', '2015-04-17', '7864489', '0.00');
+INSERT INTO `buy` VALUES ('21', '2', '2015-03-05', '2015-04-17', '7864489', '432.00');
 INSERT INTO `buy` VALUES ('22', '2', '2015-03-11', '2015-04-17', '7864489', '724.00');
-INSERT INTO `buy` VALUES ('23', '2', '2015-03-05', '2015-04-17', '02365985', '0.00');
-INSERT INTO `buy` VALUES ('24', '2', '2015-03-05', '2015-04-22', '7864489', '122.40');
+INSERT INTO `buy` VALUES ('23', '2', '2015-03-05', '2015-04-17', '02365985', '342.23');
 INSERT INTO `buy` VALUES ('25', '2', '2015-03-05', '2015-04-22', '02365985', '357.45');
+INSERT INTO `buy` VALUES ('26', '3', '2015-05-11', '2015-04-27', '1769845', '1748.29');
+INSERT INTO `buy` VALUES ('27', '4', '2015-04-01', '2015-04-27', '168945', '1365.00');
+INSERT INTO `buy` VALUES ('28', '1', '2015-05-11', '2015-05-11', '45666', '2404.02');
 
 -- ----------------------------
 -- Table structure for `customer`
@@ -82,7 +83,7 @@ CREATE TABLE `customer` (
   `date_update` date DEFAULT NULL,
   `status` smallint(1) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of customer
@@ -90,7 +91,32 @@ CREATE TABLE `customer` (
 INSERT INTO `customer` VALUES ('1', 'Maycon', 'Donizete', 'braz izzo', '435', 'centro', '86990', 'marialva', 'PR', '0003235665', '11204564005', '', '0', '2015-03-13', '2015-03-17', '0');
 INSERT INTO `customer` VALUES ('4', 'Camila', 'Cavalcante', '', '', '', '0', '', '', '1231212123', '12113213332', '', '1', '2015-03-13', '2015-03-16', '0');
 INSERT INTO `customer` VALUES ('7', 'Pedro', 'Pereira Pedreira', 'rua joao vieira paulino', '4356', 'centro', '87990655', 'maringa', 'pr', '4435353033', '4432323689', 'maycon@hotmail.com', '0', '2015-03-17', '2015-03-17', '0');
-INSERT INTO `customer` VALUES ('8', 'Saraiva', 'Ltda', 'rua jesus jenuino', '789', 'custodios', '86998789', 'maringa', 'PR', '44325669999', '44999984545', 'joaopp@gmail.xom', '1', '2015-03-17', '2015-03-17', '0');
+INSERT INTO `customer` VALUES ('8', 'Lolo', 'Ltda', 'rua jesus jenuino', '789', 'custodios', '86998789', 'maringa', 'PR', '44325669999', '44999984545', 'joaopp@gmail.xom', '1', '2015-03-17', '2015-04-27', '0');
+INSERT INTO `customer` VALUES ('9', 'Antonio', 'Arantes do Nascimento', 'rua braz izzo', '435', 'centro', '86990000', 'marialva', 'pr', '4432325669', '4456889999', 'aan@hotmail.xok', '0', '2015-04-27', '2015-04-27', '0');
+
+-- ----------------------------
+-- Table structure for `environment`
+-- ----------------------------
+DROP TABLE IF EXISTS `environment`;
+CREATE TABLE `environment` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(30) NOT NULL,
+  `order` int(11) NOT NULL,
+  `date` date NOT NULL,
+  PRIMARY KEY (`id`,`date`),
+  KEY `id` (`id`),
+  KEY `environment_order_fk` (`order`),
+  CONSTRAINT `environment_order_fk` FOREIGN KEY (`order`) REFERENCES `order` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of environment
+-- ----------------------------
+INSERT INTO `environment` VALUES ('1', 'Cozinha', '2', '0000-00-00');
+INSERT INTO `environment` VALUES ('2', 'Quarto', '2', '2015-06-05');
+INSERT INTO `environment` VALUES ('3', 'Ambiente', '2', '2015-06-05');
+INSERT INTO `environment` VALUES ('4', 'Ambiente', '10', '2015-06-05');
+INSERT INTO `environment` VALUES ('5', 'Ambiente', '10', '2015-06-05');
 
 -- ----------------------------
 -- Table structure for `fisic_person`
@@ -104,13 +130,14 @@ CREATE TABLE `fisic_person` (
   PRIMARY KEY (`id`),
   KEY `fk_fisic_person_customer_1` (`id_customer`),
   CONSTRAINT `fk_fisic_person_customer_1` FOREIGN KEY (`id_customer`) REFERENCES `customer` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of fisic_person
 -- ----------------------------
 INSERT INTO `fisic_person` VALUES ('1', '1', '07410951907', '30153530');
 INSERT INTO `fisic_person` VALUES ('4', '7', '07410951907', '122324566456');
+INSERT INTO `fisic_person` VALUES ('5', '9', '07410951907', '026559988');
 
 -- ----------------------------
 -- Table structure for `legal_person`
@@ -131,6 +158,71 @@ CREATE TABLE `legal_person` (
 -- ----------------------------
 INSERT INTO `legal_person` VALUES ('3', '4', '02944891000160', '32552580');
 INSERT INTO `legal_person` VALUES ('5', '8', '02944891000160', '4524566442452');
+
+-- ----------------------------
+-- Table structure for `order`
+-- ----------------------------
+DROP TABLE IF EXISTS `order`;
+CREATE TABLE `order` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `customer` int(11) NOT NULL,
+  `date_register` date NOT NULL,
+  `status` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `order_customer-fk` (`customer`),
+  KEY `order_status_fk` (`status`),
+  CONSTRAINT `order_customer-fk` FOREIGN KEY (`customer`) REFERENCES `customer` (`id`),
+  CONSTRAINT `order_status_fk` FOREIGN KEY (`status`) REFERENCES `status_order` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of order
+-- ----------------------------
+INSERT INTO `order` VALUES ('2', '1', '2015-06-01', '1');
+INSERT INTO `order` VALUES ('3', '4', '2015-06-01', '1');
+INSERT INTO `order` VALUES ('4', '7', '2015-06-01', '1');
+INSERT INTO `order` VALUES ('7', '4', '2015-06-02', '1');
+INSERT INTO `order` VALUES ('9', '7', '2015-06-02', '1');
+INSERT INTO `order` VALUES ('10', '1', '2015-06-02', '1');
+INSERT INTO `order` VALUES ('11', '1', '2015-06-02', '1');
+INSERT INTO `order` VALUES ('12', '1', '2015-06-02', '1');
+INSERT INTO `order` VALUES ('13', '1', '2015-06-03', '1');
+INSERT INTO `order` VALUES ('14', '1', '2015-06-03', '1');
+
+-- ----------------------------
+-- Table structure for `product_order`
+-- ----------------------------
+DROP TABLE IF EXISTS `product_order`;
+CREATE TABLE `product_order` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `description` varchar(255) NOT NULL,
+  `qtd` int(11) NOT NULL DEFAULT '0',
+  `value` decimal(10,2) DEFAULT NULL,
+  `environment` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `product_environment_fk` (`environment`),
+  CONSTRAINT `product_environment_fk` FOREIGN KEY (`environment`) REFERENCES `environment` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of product_order
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for `status_order`
+-- ----------------------------
+DROP TABLE IF EXISTS `status_order`;
+CREATE TABLE `status_order` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `description` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of status_order
+-- ----------------------------
+INSERT INTO `status_order` VALUES ('1', 'Orçamento');
+INSERT INTO `status_order` VALUES ('2', 'Pedido');
 
 -- ----------------------------
 -- Table structure for `supplier`
